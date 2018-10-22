1d1eb4ecbbc1732c680c3175ab6e4cfb5fa1bd3f
Jacques Begin <jacques.andre.begin@gmail.com>
2018-10-22 19:07:10 -0400

    Adds gulp-watch package and updates gulpfile.js.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-var gulp = require('gulp');
+var { gulp, watch } = require('gulp');
 
 // gulp.task('default', function(){
 //   console.log("Gulp task created");
@@ -15,5 +15,14 @@ function html(cb) {
   cb();
 }
 
+function watch(cb) {
+  watch('./app/index.html', function() {
+    html(cb);
+  });
+}
+
+// watch('./app/index.html', html);
+
 exports.html = html;
 exports.default = test;
+exports.watch = watch;
